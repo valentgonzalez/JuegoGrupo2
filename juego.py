48598e05378543a2240793a0d82e34f8f867dd28
DonMaluna <101161129+DonMaluna@users.noreply.github.com>
2022-03-13 23:11:52 -0300

    resultado, variable y verificador

--- juego.py
+++ juego.py
@@ -1,4 +1,5 @@
 
+
 def resultado(n1, n2, n3):
   if (n3 == 0):
     if (n1 < n2):
@@ -11,9 +12,28 @@ def resultado(n1, n2, n3):
     if (n1 < n2):
       print ("Perdiste")
 
-inicio()
-numero = int(input("Ingresa un número del 1 al 10: "))
-rango = int(input("Ingresa un 0 si crees que tu número es menor que el aleatorio o un 1 si crees que es mayor: "))
+verificador = 0 
 num_random = random.randint(1, 10)
-funcion_random(numero, num_random)
-resultado(numero, num_random, rango)
+inicio()
+
+while verificador == 0:
+    numero = int(input("Ingresa un número del 1 al 10: "))
+    if numero < 11 and numero > 1 :
+         rango = int(input("Ingresa un 0 si crees que tu número es menor que el aleatorio o un 1 si crees que es mayor: "))
+         funcion_random(numero, num_random)
+         resultado(numero, num_random, rango)
+         verificador = 1 
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
